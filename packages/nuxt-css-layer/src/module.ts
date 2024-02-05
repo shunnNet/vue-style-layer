3103a2c1683380845fdad8a6bbba2c5f7931b275
net <wendell20904102@gmail.com>
2024-02-06 00:30:56 +0800

    feat: support unplugin changes in nuxt module

--- packages/nuxt-css-layer/src/module.ts
+++ packages/nuxt-css-layer/src/module.ts
@@ -3,12 +3,18 @@ import {
   addServerPlugin,
   createResolver,
   addVitePlugin,
+  addWebpackPlugin,
 } from "@nuxt/kit"
-import VueStyleLayer from "unplugin-vue-style-layer"
+import VueStyleLayerVite from "unplugin-vue-style-layer/vite"
+import VueStyleLayerWebpack from "unplugin-vue-style-layer/webpack"
 import defu from "defu"
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  componentLayer: string
+  injectOrder: boolean | string[]
+  order: string[]
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
@@ -16,15 +22,28 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: "cssLayer",
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
+  defaults: {
+    componentLayer: "components",
+    injectOrder: false,
+    order: [],
+  },
   setup(options, nuxt) {
-    const _options = defu(options, {
-      styleLayer: {},
-      order: [],
-    })
+    const _options = defu(options, {})
     const resolver = createResolver(import.meta.url)
+    const injectOrder: string[] =
+      _options.injectOrder === true
+        ? _options.order
+        : Array.isArray(_options.injectOrder)
+          ? _options.injectOrder
+          : []
+    const pluginOptions = {
+      componentLayer: _options.componentLayer,
+      order: [],
+      injectOrder,
+    }
     addServerPlugin(resolver.resolve("./runtime/server-plugin"))
-    addVitePlugin(VueStyleLayer.vite(_options.styleLayer))
+    addVitePlugin(VueStyleLayerVite(pluginOptions))
+    addWebpackPlugin(VueStyleLayerWebpack(pluginOptions))
 
     nuxt.options.runtimeConfig.cssLayer = { order: _options.order }
   },
